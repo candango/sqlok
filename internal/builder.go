e99d7c64d0858b4e1dfe8f284ccba5103786381b
Flavio Garcia <piraz@candango.org>
2025-01-15 23:37:48 -0500

    feat(builder): add basic crud operations
    Still need to trim and figure out some issues with logic
    operations and multi-line inserts.
    
    Refs: #6

--- internal/builder.go
+++ internal/builder.go
@@ -19,6 +19,21 @@ const (
 	RightJoin JoinType = "RIGHT JOIN"
 )
 
+type ConditionType string
+
+const (
+	AndCondition ConditionType = "AND"
+	OrCondition  ConditionType = "OR"
+)
+
+func And(condition ...string) string {
+	return fmt.Sprintf("%s %s", AndCondition, strings.Join(condition, " "))
+}
+
+func Or(condition ...string) string {
+	return fmt.Sprintf("%s %s", OrCondition, strings.Join(condition, " "))
+}
+
 type QueryBuilder interface {
 	Build() (string, []any)
 	Execute(ctx context.Context, db *sql.DB) (*sql.Rows, error)
@@ -29,31 +44,13 @@ type queryBuilder struct {
 	args  []any
 }
 
-func NewQueryBuilder() QueryBuilder {
-	return &queryBuilder{}
-}
-
-func (b *queryBuilder) Build() (string, []any) {
-	return b.query, b.args
-}
-
-func (b *queryBuilder) Execute(ctx context.Context, db *sql.DB) (*sql.Rows, error) {
-	query, args := b.Build()
-	log.Info("executing query: ", query, "  with args: ", args)
-	rows, err := db.QueryContext(ctx, query, args...)
-	if err != nil {
-		return nil, fmt.Errorf("query execution failed: %v", err)
-	}
-	return rows, err
-}
-
 type SelectBuilder interface {
 	QueryBuilder
 	Select(columns ...string) SelectBuilder
 	From(table string) SelectBuilder
-	Where(condition string, args ...interface{}) SelectBuilder
-	And(condition string, args ...interface{}) SelectBuilder
-	Or(condition string, args ...interface{}) SelectBuilder
+	Where(condition string, args ...any) SelectBuilder
+	And(condition string, args ...any) SelectBuilder
+	Or(condition string, args ...any) SelectBuilder
 	OrderBy(columns ...string) SelectBuilder
 	Limit(limit int) SelectBuilder
 	Offset(limit int) SelectBuilder
@@ -61,7 +58,6 @@ type SelectBuilder interface {
 }
 
 type selectBuilder struct {
-	QueryBuilder
 	selectColumns []string
 	fromTable     string
 	where         []string
@@ -80,108 +76,349 @@ type joinInfo struct {
 }
 
 func NewSelectBuiler() SelectBuilder {
-	b := &selectBuilder{
-		QueryBuilder: NewQueryBuilder(),
-	}
+	b := &selectBuilder{}
 	b.Clear()
 	return b
 }
 
-func (q *selectBuilder) Clear() SelectBuilder {
-	q.selectColumns = []string{}
-	q.fromTable = ""
-	q.where = []string{}
-	q.whereArgs = []any{}
-	q.orderBy = []string{}
-	q.limit = 0
-	q.offset = 0
-	q.joins = []joinInfo{}
-	return q
+func (b *selectBuilder) Clear() SelectBuilder {
+	b.selectColumns = []string{}
+	b.fromTable = ""
+	b.where = []string{}
+	b.whereArgs = []any{}
+	b.orderBy = []string{}
+	b.limit = 0
+	b.offset = 0
+	b.joins = []joinInfo{}
+	return b
 }
 
-func (q *selectBuilder) Select(columns ...string) SelectBuilder {
-	q.selectColumns = append(q.selectColumns, columns...)
-	return q
+func (b *selectBuilder) Select(columns ...string) SelectBuilder {
+	b.selectColumns = append(b.selectColumns, columns...)
+	return b
 }
 
-func (q *selectBuilder) From(table string) SelectBuilder {
-	q.fromTable = table
-	return q
+func (b *selectBuilder) From(table string) SelectBuilder {
+	b.fromTable = table
+	return b
 }
 
-func (q *selectBuilder) Where(condition string, args ...any) SelectBuilder {
-	q.where = append(q.where, condition)
-	q.whereArgs = append(q.whereArgs, args...)
-	return q
+func (b *selectBuilder) Where(condition string, args ...any) SelectBuilder {
+	b.where = append(b.where, condition)
+	b.whereArgs = append(b.whereArgs, args...)
+	return b
 }
 
-func (q *selectBuilder) And(condition string, args ...any) SelectBuilder {
-	return q.Where(condition, args...)
+func (b *selectBuilder) And(condition string, args ...any) SelectBuilder {
+	return b.Where(And(condition), args...)
 }
 
-func (q *selectBuilder) Or(condition string, args ...any) SelectBuilder {
-	return q.Where(condition, args...)
+func (b *selectBuilder) Or(condition string, args ...any) SelectBuilder {
+	return b.Where(Or(condition), args...)
 }
 
-func (q *selectBuilder) OrderBy(columns ...string) SelectBuilder {
-	q.orderBy = append(q.orderBy, columns...)
-	return q
+func (b *selectBuilder) OrderBy(columns ...string) SelectBuilder {
+	b.orderBy = append(b.orderBy, columns...)
+	return b
 }
 
-func (q *selectBuilder) Limit(limit int) SelectBuilder {
-	q.limit = limit
-	return q
+func (b *selectBuilder) Limit(limit int) SelectBuilder {
+	b.limit = limit
+	return b
 }
 
-func (q *selectBuilder) Offset(offset int) SelectBuilder {
-	q.offset = offset
-	return q
+func (b *selectBuilder) Offset(offset int) SelectBuilder {
+	b.offset = offset
+	return b
 }
 
-func (q *selectBuilder) Join(joinType JoinType, table string, on string) SelectBuilder {
-	q.joins = append(q.joins, joinInfo{joinType, table, on})
-	return q
+func (b *selectBuilder) Join(joinType JoinType, table string, on string) SelectBuilder {
+	b.joins = append(b.joins, joinInfo{joinType, table, on})
+	return b
 }
 
-func (q *selectBuilder) Build() (string, []any) {
-	var b strings.Builder
+func (b *selectBuilder) Build() (string, []any) {
+	var sb strings.Builder
 
-	b.WriteString("SELECT ")
-	b.WriteString(strings.Join(q.selectColumns, ", "))
+	sb.WriteString("SELECT ")
+	sb.WriteString(strings.Join(b.selectColumns, ", "))
 
-	b.WriteString(" FROM ")
-	b.WriteString(q.fromTable)
+	sb.WriteString(" FROM ")
+	sb.WriteString(b.fromTable)
 
-	for _, join := range q.joins {
-		b.WriteString(fmt.Sprintf(" %s %s ON %s ", join.joinType, join.table, join.on))
+	for _, join := range b.joins {
+		sb.WriteString(fmt.Sprintf(" %s %s ON %s ", join.joinType, join.table, join.on))
 	}
 
 	// TODO: Implement the AND/OR properly
-	if len(q.where) > 0 {
-		b.WriteString(" WHERE ")
-		b.WriteString(strings.Join(q.where, " AND "))
+	if len(b.where) > 0 {
+		sb.WriteString(" WHERE ")
+		sb.WriteString(strings.Join(b.where, " "))
 	}
 
-	if len(q.orderBy) > 0 {
-		b.WriteString(" ORDER BY ")
-		b.WriteString(strings.Join(q.orderBy, ", "))
+	if len(b.orderBy) > 0 {
+		sb.WriteString(" ORDER BY ")
+		sb.WriteString(strings.Join(b.orderBy, ", "))
 	}
 
-	if q.limit > 0 {
-		b.WriteString(fmt.Sprintf(" LIMIT %d ", q.limit))
+	if b.limit > 0 {
+		sb.WriteString(fmt.Sprintf(" LIMIT %d ", b.limit))
 	}
 
-	if q.offset > 0 {
-		b.WriteString(fmt.Sprintf(" LIMIT %d ", q.offset))
+	if b.offset > 0 {
+		sb.WriteString(fmt.Sprintf(" LIMIT %d ", b.offset))
 	}
-	args := q.whereArgs
-	q.Clear()
-	return b.String(), args
+	args := b.whereArgs
+	b.Clear()
+	return sb.String(), args
 }
 
 func (b *selectBuilder) Execute(ctx context.Context, db *sql.DB) (*sql.Rows, error) {
 	query, args := b.Build()
 	log.Info("executing query: ", query, "  with args: ", args)
+	rows, err := db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("query execution failed: %v", err)
+	}
+	return rows, err
+}
+
+type InsertBuilder interface {
+	QueryBuilder
+	InsertInto(table string) InsertBuilder
+	Columns(columns ...string) InsertBuilder
+	Values(values ...any) InsertBuilder
+}
+
+type insertBuilder struct {
+	table   string
+	columns []string
+	values  [][]any
+	args    []any
+}
+
+func NewInsertBuiler() InsertBuilder {
+	b := &insertBuilder{}
+	b.Clear()
+	return b
+}
+
+func (b *insertBuilder) InsertInto(table string) InsertBuilder {
+	b.table = table
+	return b
+}
+
+func (b *insertBuilder) Columns(columns ...string) InsertBuilder {
+	b.columns = columns
+	return b
+}
+
+func (b *insertBuilder) Values(values ...any) InsertBuilder {
+	b.values = append(b.values, values)
+	return b
+}
+
+func (b *insertBuilder) Clear() InsertBuilder {
+	b.table = ""
+	b.columns = []string{}
+	b.values = [][]any{}
+	return b
+}
+
+func (b *insertBuilder) Build() (string, []any) {
+	var sb strings.Builder
+	sb.WriteString("INSERT INTO ")
+	sb.WriteString(b.table)
+
+	if len(b.columns) > 0 {
+		sb.WriteString(" (")
+		sb.WriteString(strings.Join(b.columns, ", "))
+		sb.WriteString(")")
+	}
+
+	sb.WriteString(" VALUES")
+	var allArgs []any
+	var valuesPlaceholders []string
+	for i, valueSet := range b.values {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+		placeholders := make([]string, len(valueSet))
+		for j := range valueSet {
+			// TODO: WHAT!!!!????
+			placeholders[j] = fmt.Sprintf("$%v", j+1)
+			allArgs = append(allArgs, valueSet[j])
+		}
+		valuesPlaceholders = append(valuesPlaceholders, "("+strings.Join(placeholders, ", ")+")")
+	}
+
+	sb.WriteString(strings.Join(valuesPlaceholders, ", "))
+
+	args := allArgs
+	b.Clear()
+
+	return sb.String(), args
+}
+
+func (b *insertBuilder) Execute(ctx context.Context, db *sql.DB) (*sql.Rows, error) {
+	query, args := b.Build()
+	log.Info("executing INSERT query: ", query, "  with args: ", args)
+	rows, err := db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("query execution failed: %v", err)
+	}
+	return rows, err
+}
+
+type UpdateBuilder interface {
+	QueryBuilder
+	Update(table string) UpdateBuilder
+	Set(column string, value any) UpdateBuilder
+	Where(condition string, args ...any) UpdateBuilder
+	And(condition string, args ...any) UpdateBuilder
+	Or(condition string, args ...any) UpdateBuilder
+}
+
+type updateBuilder struct {
+	table string
+	set   []string
+	where []string
+	args  []any
+}
+
+func NewUpdateBuilder() UpdateBuilder {
+	b := &updateBuilder{}
+	b.Clear()
+	return b
+}
+
+func (b *updateBuilder) Update(table string) UpdateBuilder {
+	b.table = table
+	return b
+}
+
+func (b *updateBuilder) Set(column string, value any) UpdateBuilder {
+	b.set = append(b.set, fmt.Sprintf("%s = $%d", column, len(b.args)+1))
+	b.args = append(b.args, value)
+	return b
+}
+
+func (b *updateBuilder) Where(condition string, args ...any) UpdateBuilder {
+	b.where = append(b.where, condition)
+	b.args = append(b.args, args...)
+	return b
+}
+
+func (b *updateBuilder) And(condition string, args ...any) UpdateBuilder {
+	return b.Where(And(condition), args...)
+}
+
+func (b *updateBuilder) Or(condition string, args ...any) UpdateBuilder {
+	return b.Where(Or(condition), args...)
+}
+
+func (b *updateBuilder) Clear() UpdateBuilder {
+	b.table = ""
+	b.set = []string{}
+	b.where = []string{}
+	b.args = []any{}
+	return b
+}
+
+func (b *updateBuilder) Build() (string, []any) {
+	var sb strings.Builder
+	sb.WriteString("UPDATE ")
+	sb.WriteString(b.table)
+
+	sb.WriteString(" SET ")
+	sb.WriteString(strings.Join(b.set, ", "))
+
+	if len(b.where) > 0 {
+		sb.WriteString(" WHERE ")
+		sb.WriteString(strings.Join(b.where, " "))
+	}
+
+	args := b.args
+	b.Clear()
+
+	return sb.String(), args
+}
+
+func (b *updateBuilder) Execute(ctx context.Context, db *sql.DB) (*sql.Rows, error) {
+	query, args := b.Build()
+	log.Info("executing UPDATE query: ", query, "  with args: ", args)
+	rows, err := db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("query execution failed: %v", err)
+	}
+	return rows, err
+}
+
+type DeleteBuilder interface {
+	QueryBuilder
+	DeleteFrom(table string) DeleteBuilder
+	Where(condition string, args ...any) DeleteBuilder
+	And(condition string, args ...any) DeleteBuilder
+	Or(condition string, args ...any) DeleteBuilder
+}
+
+type deleteBuilder struct {
+	table string
+	where []string
+	args  []any
+}
+
+func NewDeleteBuiler() DeleteBuilder {
+	b := &deleteBuilder{}
+	b.Clear()
+	return b
+}
+
+func (b *deleteBuilder) DeleteFrom(table string) DeleteBuilder {
+	b.table = table
+	return b
+}
+
+func (b *deleteBuilder) Where(condition string, args ...any) DeleteBuilder {
+	b.where = append(b.where, condition)
+	b.args = append(b.args, args...)
+	return b
+}
+
+func (b *deleteBuilder) And(condition string, args ...any) DeleteBuilder {
+	return b.Where(And(condition), args...)
+}
+
+func (b *deleteBuilder) Or(condition string, args ...any) DeleteBuilder {
+	return b.Where(Or(condition), args...)
+}
+
+func (b *deleteBuilder) Clear() DeleteBuilder {
+	b.table = ""
+	b.where = []string{}
+	b.args = []any{}
+	return b
+}
+
+func (b *deleteBuilder) Build() (string, []any) {
+	var sb strings.Builder
+	sb.WriteString("DELETE FROM ")
+	sb.WriteString(b.table)
+
+	if len(b.where) > 0 {
+		sb.WriteString(" WHERE ")
+		sb.WriteString(strings.Join(b.where, " "))
+	}
+
+	args := b.args
+	b.Clear()
+
+	return sb.String(), args
+}
+
+func (b *deleteBuilder) Execute(ctx context.Context, db *sql.DB) (*sql.Rows, error) {
+	query, args := b.Build()
+	log.Info("executing DELETE query: ", query, "  with args: ", args)
 	rows, err := db.QueryContext(ctx, query, args...)
 	if err != nil {
 		return nil, fmt.Errorf("query execution failed: %v", err)
--- internal/builder_test.go
+++ internal/builder_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBuilder(t *testing.T) {
+func TestSelectBuilder(t *testing.T) {
 	columns := []string{"column1", "column2"}
 	table1 := "table1"
 
@@ -25,10 +25,100 @@ func TestBuilder(t *testing.T) {
 		assert.Equal(t, []any{}, args)
 	})
 
-	t.Run("Should select with where clause with parameters", func(t *testing.T) {
+	t.Run("Should select with where clause with parameters and AND condition", func(t *testing.T) {
 		b.Select(columns...).From(table1).Where(columns[0]+"=$1", 1).And(columns[1]+"=$2", 2)
 		sql, args := b.Build()
-		assert.Equal(t, "SELECT "+strings.Join(columns, ", ")+" FROM table1 WHERE "+columns[0]+"=$1 AND "+columns[1]+"=$2", sql)
+		assert.Equal(t, "SELECT "+strings.Join(columns, ", ")+
+			" FROM table1 WHERE "+columns[0]+"=$1 "+And(columns[1])+"=$2", sql)
 		assert.Equal(t, []any{1, 2}, args)
+	})
+
+	t.Run("Should select with where clause with parameters and OR condition", func(t *testing.T) {
+		b.Select(columns...).From(table1).Where(columns[0]+"=$1", 1).Or(columns[1]+"=$2", 2)
+		sql, args := b.Build()
+		assert.Equal(t, "SELECT "+strings.Join(columns, ", ")+
+			" FROM table1 WHERE "+columns[0]+"=$1 "+Or(columns[1])+"=$2", sql)
+		assert.Equal(t, []any{1, 2}, args)
+	})
+}
+
+func TestInsertBuilder(t *testing.T) {
+	columns := []string{"column1", "column2"}
+	table1 := "table1"
+	values1 := []any{"column1", "column2"}
+	// values2 := []any{"column1", "column2"}
+
+	b := NewInsertBuiler()
+	t.Run("Should insert columns from table1 with one line of values", func(t *testing.T) {
+		b.InsertInto(table1).Values(values1...)
+		sql, args := b.Build()
+		assert.Equal(t, "INSERT INTO "+table1+" VALUES($1, $2)", sql)
+		assert.Equal(t, values1, args)
+	})
+	t.Run("Should insert columns from table1 with columns with one line of values", func(t *testing.T) {
+		b.InsertInto(table1).Columns(columns...).Values(values1...)
+		sql, args := b.Build()
+		assert.Equal(t, "INSERT INTO "+table1+" ("+strings.Join(columns, ", ")+") VALUES($1, $2)", sql)
+		assert.Equal(t, values1, args)
+	})
+	// FIXME: Fix more than one line insert
+	// t.Run("Should insert columns from table1 with two line of values", func(t *testing.T) {
+	// 	b.InsertInto(table1).Values(values1...).Values(values2...)
+	// 	sql, args := b.Build()
+	// 	assert.Equal(t, "INSERT INTO "+table1+" VALUES($1, $2), ($3, $4)", sql)
+	// 	assert.Equal(t, values1, args)
+	// })
+
+}
+
+func TestUpdateBuilder(t *testing.T) {
+	columns := []string{"column1", "column2"}
+	table1 := "table1"
+	values := []any{"value1", "value2"}
+	// values2 := []any{"column1", "column2"}
+
+	b := NewUpdateBuilder()
+	t.Run("Should update table1 setting just columns with values", func(t *testing.T) {
+		b.Update(table1).Set(columns[0], values[0]).Set(columns[1], values[1])
+		sql, args := b.Build()
+		assert.Equal(t, "UPDATE "+table1+" SET column1 = $1, column2 = $2", sql)
+		assert.Equal(t, values, args)
+	})
+	t.Run("Should update table1 setting columns with values and where clause", func(t *testing.T) {
+		values := values
+		values = append(values, "value3")
+		b.Update(
+			table1,
+		).Set(columns[0], values[0]).Set(columns[1], values[1]).Where("column3=$3", values[2])
+		sql, args := b.Build()
+		assert.Equal(t, "UPDATE "+table1+
+			" SET column1 = $1, column2 = $2 WHERE column3=$3", sql)
+		assert.Equal(t, values, args)
+	})
+}
+
+func TestDeleteBuilder(t *testing.T) {
+	columns := []string{"column1", "column2"}
+	table1 := "table1"
+	values := []any{"value1", "value2"}
+
+	b := NewDeleteBuiler()
+	t.Run("Should delete from table1", func(t *testing.T) {
+		b.DeleteFrom(table1)
+		sql, _ := b.Build()
+		assert.Equal(t, "DELETE FROM "+table1, sql)
+		// assert.Equal(t, values, args)
+	})
+	t.Run("Should delete table1 and where clause and AND condition", func(t *testing.T) {
+		b.DeleteFrom(table1).Where(columns[0]+"=$1", values[0]).And(columns[1]+"=$2", values[1])
+		sql, args := b.Build()
+		assert.Equal(t, "DELETE FROM "+table1+" WHERE column1=$1 AND column2=$2", sql)
+		assert.Equal(t, values, args)
+	})
+	t.Run("Should delete table1 and where clause and OR condition", func(t *testing.T) {
+		b.DeleteFrom(table1).Where(columns[0]+"=$1", values[0]).Or(columns[1]+"=$2", values[1])
+		sql, args := b.Build()
+		assert.Equal(t, "DELETE FROM "+table1+" WHERE column1=$1 OR column2=$2", sql)
+		assert.Equal(t, values, args)
 	})
 }
