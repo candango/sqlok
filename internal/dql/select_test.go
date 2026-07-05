32a74d762ba4e25dbf73df08fc40a7dd16b83296
Flavio Garcia <piraz@candango.org>
2026-07-05 01:29:12 -0400

    test(select): cover visitor traversal
    Refactor SELECT AST tests to use a traversing visitor that walks
    projected columns and their column reference expressions.
    
    Refs: #6

--- internal/dql/select_test.go
+++ internal/dql/select_test.go
@@ -3,13 +3,15 @@ package dql
 import (
 	"testing"
 
+	"github.com/candango/sqlok/internal/elements"
 	"github.com/candango/sqlok/internal/sst"
 	"github.com/stretchr/testify/assert"
 )
 
 type fakeVisitor struct {
-	visitedSelect  bool
-	visitedColumns int
+	visitedSelect     bool
+	visitedColumns    int
+	visitedColumnRefs int
 }
 
 func (v *fakeVisitor) VisitSelect(s sst.SelectNode) error {
@@ -23,12 +25,39 @@ func (v *fakeVisitor) VisitSelectColumn(s sst.SelectColumnNode) error {
 }
 
 func (v *fakeVisitor) VisitColumnRef(s sst.ColumnRefNode) error {
+	v.visitedColumnRefs++
 	return nil
 }
 
 type fakeExpr struct{}
 
 func (e *fakeExpr) Accept(v sst.Visitor) error {
+	return nil
+}
+
+type traversingVisitor struct {
+	visitedSelect     bool
+	visitedColumns    int
+	visitedColumnRefs int
+}
+
+func (v *traversingVisitor) VisitSelect(s sst.SelectNode) error {
+	v.visitedSelect = true
+	for _, column := range s.Columns() {
+		if err := column.Accept(v); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (v *traversingVisitor) VisitSelectColumn(s sst.SelectColumnNode) error {
+	v.visitedColumns++
+	return s.Expr().Accept(v)
+}
+
+func (v *traversingVisitor) VisitColumnRef(s sst.ColumnRefNode) error {
+	v.visitedColumnRefs++
 	return nil
 }
 
@@ -45,15 +74,18 @@ func TestSelectAcceptVisitsSelect(t *testing.T) {
 	}
 }
 
-func TestNewSelectStoresColumns(t *testing.T) {
-	visitor := &fakeVisitor{}
-	col1 := NewSelectColumn(&fakeExpr{})
-	col2 := NewSelectColumn(&fakeExpr{})
-	selectNode := NewSelect(col1, col2)
+func TestSelectColumnRefChainCanBeVisited(t *testing.T) {
+	visitor := &traversingVisitor{}
+	columnRef := elements.NewColumnRef("users", "id").WithSchema("public")
+	selectNode := NewSelect(NewSelectColumn(columnRef))
 
-	assert.Len(t, selectNode.Columns(), 2)
-	for _, col := range selectNode.Columns() {
-		assert.NoError(t, col.Accept(visitor))
-	}
-	assert.Equal(t, 2, visitor.visitedColumns)
+	assert.Len(t, selectNode.Columns(), 1)
+	assert.NoError(t, selectNode.Accept(visitor))
+
+	assert.True(t, visitor.visitedSelect)
+	assert.Equal(t, 1, visitor.visitedColumns)
+	assert.Equal(t, 1, visitor.visitedColumnRefs)
+	assert.Equal(t, "public", columnRef.Schema())
+	assert.Equal(t, "users", columnRef.Table())
+	assert.Equal(t, "id", columnRef.Name())
 }
